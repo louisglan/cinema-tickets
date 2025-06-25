8a69fe97856d0c15002d5b1e6a187e37ad323535
Louis <louissam@hotmail.co.uk>
2025-06-25 12:17:08 +0100

    Added payments and seat reservation for 1 adult

--- cinema-tickets-javascript/src/pairtest/TicketService.js
+++ cinema-tickets-javascript/src/pairtest/TicketService.js
@@ -1,12 +1,26 @@
 import TicketTypeRequest from './lib/TicketTypeRequest.js';
 import InvalidPurchaseException from './lib/InvalidPurchaseException.js';
+import TicketPaymentService from '../thirdparty/paymentgateway/TicketPaymentService.js';
+import SeatReservationService from '../thirdparty/seatbooking/SeatReservationService.js';
 
 export default class TicketService {
+  #ticketPaymentService = new TicketPaymentService()
+  #seatReservationService = new SeatReservationService()
   /**
    * Should only have private methods other than the one below.
    */
 
   purchaseTickets(accountId, ...ticketTypeRequests) {
-    // throws InvalidPurchaseException
+    this.#ticketPaymentService.makePayment(accountId, this.#getCostFromTicketType(ticketTypeRequests[0].getTicketType()))
+    this.#seatReservationService.reserveSeat(accountId, ticketTypeRequests[0].getNoOfTickets())
+  }
+
+  #getCostFromTicketType(ticketType) {
+    const costs = {
+      'ADULT': 25,
+      'CHILD': 15,
+      'INFANT': 0
+    }
+    return costs[ticketType]
   }
 }
--- cinema-tickets-javascript/test/TicketService.test.js
+++ cinema-tickets-javascript/test/TicketService.test.js
@@ -1,7 +1,31 @@
+import TicketTypeRequest from "../src/pairtest/lib/TicketTypeRequest";
 import TicketService from "../src/pairtest/TicketService"
+import TicketPaymentService from "../src/thirdparty/paymentgateway/TicketPaymentService"
+import SeatReservationService from "../src/thirdparty/seatbooking/SeatReservationService";
 
 const ticketService = new TicketService;
+const ADULT = 'ADULT'
+const CHILD = 'CHILD'
+const INFANT = 'INFANT'
+const ACCOUNT_ID = 999999
+jest.mock("../src/thirdparty/paymentgateway/TicketPaymentService")
+jest.mock("../src/thirdparty/seatbooking/SeatReservationService")
 
-test("blank", () => {
-    
+test("One adult ticket makes payment of £25 and Reserves one Seat", () => {
+    const adultTicketRequest = new TicketTypeRequest(ADULT, 1)
+    ticketService.purchaseTickets(ACCOUNT_ID, adultTicketRequest)
+    const mockMakePayment = mockMakePaymentMethod()
+    const mockReserveSeat = mockReserveSeatMethod()
+    expect(mockMakePayment).toHaveBeenCalledWith(ACCOUNT_ID, 25)
+    expect(mockReserveSeat).toHaveBeenCalledWith(ACCOUNT_ID, 1)
 })
+
+function mockMakePaymentMethod() {
+    const mockTicketPaymentServiceInstance = TicketPaymentService.mock.instances[0];
+    return mockTicketPaymentServiceInstance.makePayment;
+}
+
+function mockReserveSeatMethod() {
+    const mockSeatReservationServiceInstance = SeatReservationService.mock.instances[0];
+    return mockSeatReservationServiceInstance.reserveSeat;
+}
